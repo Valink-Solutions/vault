c26bfa40713547b5350ebfa0866beb591d21b90d
JakePIXL <jakewjevans@gmail.com>
2023-04-27 14:17:39 -0400

    cleaned up tasks

---
+++ src/tasks/mod.rs
@@ -0,0 +1,90 @@
+pub mod runner;
+
+use std::sync::Arc;
+
+use futures::StreamExt;
+use log::{info, warn};
+use object_store::{path::Path, ObjectStore};
+pub use runner::TaskRunner;
+
+pub async fn delete_old_sessions(pool: &sqlx::PgPool) {
+    info!("Starting old session deletion.");
+
+    let result = sqlx::query!(
+        "
+        DELETE FROM sessions
+        WHERE expires_at < $1
+        ",
+        chrono::Utc::now().naive_utc()
+    )
+    .execute(pool)
+    .await;
+
+    match result {
+        Ok(_) => info!("Successfully deleted old session records."),
+        Err(e) => warn!("Failed to delete old records from sessions: {:?}", e),
+    }
+}
+
+pub async fn delete_queued_worlds(pool: &sqlx::PgPool, object_store: &Arc<Box<dyn ObjectStore>>) {
+    info!("Starting queued world deletion.");
+
+    let result = sqlx::query!(
+        "
+        SELECT * FROM deleted_worlds
+        "
+    )
+    .fetch_all(pool)
+    .await;
+
+    match result {
+        Ok(worlds) => {
+            for world in worlds {
+                let prefix: Path = format!("{}/{}", world.user_id, world.world_id)
+                    .try_into()
+                    .unwrap();
+                match object_store.list(Some(&prefix)).await {
+                    Ok(mut stream) => {
+                        // let mut stream = stream.into_inner();
+                        while let Some(item) = stream.next().await {
+                            match item {
+                                Ok(object_meta) => {
+                                    // Perform desired operations on each ObjectMeta here
+                                    println!("ObjectMeta: {:?}", object_meta);
+
+                                    match object_store.delete(&object_meta.location).await {
+                                        Ok(_) => {}
+                                        Err(e) => {
+                                            warn!("Failed to delete queued worlds: {:?}", e)
+                                        }
+                                    };
+                                }
+                                Err(e) => {
+                                    eprintln!("Error processing item: {:?}", e);
+                                }
+                            }
+                        }
+                    }
+                    Err(e) => warn!("Failed to delete queued worlds: {:?}", e),
+                }
+
+                match sqlx::query!(
+                    "
+                    DELETE FROM deleted_worlds
+                    WHERE id = $1
+                    ",
+                    world.id
+                )
+                .execute(pool)
+                .await
+                {
+                    Ok(_) => {}
+                    Err(e) => warn!("Failed to delete queued world: {:?}", e),
+                };
+            }
+        }
+        Err(e) => warn!("Failed to delete queued worlds: {:?}", e),
+    }
+
+    info!("Successfully deleted queued worlds.")
+}
